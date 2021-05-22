bc6ea4e206fa15136c3041f04af652ee248ccdd9
namnam <namnam@coder.com>
2021-05-22 18:39:28 +0700

    Add README.md for how to add georegression library for project

--- georegression/readme.md
+++
@@ -1,1 +0,0 @@
-This is geometry library for projectb 
--- model/AbstractObject.java
+++ model/AbstractObject.java
@@ -1,6 +1,5 @@
 package model;
 import georegression.struct.point.Point3D_F32;
-
 public class AbstractObject {
 	// 8 location of object
 	Point3D_F32 p1, p2, p3, p4, p5, p6, p7, p8;
