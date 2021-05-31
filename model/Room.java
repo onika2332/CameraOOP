3ddbd3e200fdb557e80a168229b8b4210282c9b7
namnam <namnam@coder.com>
2021-05-31 22:25:54 +0700

    Add some attributes of Room class

--- model/Room.java
+++ model/Room.java
@@ -4,5 +4,36 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Room extends AbstractObject {
+	private float length;
+	private float width;
+	private float height;
+	List<Plane> walls;
+	public float getLength() {
+		return length;
+	}
+
+	public void setLength(float length) {
+		this.length = length;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public Room() {
+		super();
+	}
 	
 }
