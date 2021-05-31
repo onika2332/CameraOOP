1bfa9ac7c6557698333d18a8487d0e9b27e5d40a
onika2332 <74920500+onika2332@users.noreply.github.com>
2021-05-31 19:28:25 +0700

    Update Vector.java

--- model/Vector.java
+++ model/Vector.java
@@ -1,32 +1,28 @@
 package model;
 import georegression.struct.point.Point3D_F32;
+import georegression.struct.point.Vector3D_F32;
 
-
-public class Vector {
-	private float param1;
-	private float param2;
-	private float param3;
-	public float getParam1() {
-		return param1;
-	}
-	public float getParam2() {
-		return param2;
-	}
-	public float getParam3() {
-		return param3;
-	}
+public class Vector extends Vector3D_F32 {
 	
+	public Vector() {
+		super();
+	}
 	public Vector(float x, float y, float z) {
-		this.param1 = x;
-		this.param2 = y;
-		this.param3 = z;
+		super(x,y,z);
 	}
 	
 	public Vector(Point3D_F32 p1, Point3D_F32 p2) {
-		this.param1 = p1.getX() - p2.getX();
-		this.param2 = p1.getY() - p2.getY();
-		this.param3 = p1.getZ() - p2.getZ();
+		super(p1,p2);
+	}
+	
+	public boolean checkParallel(Vector v) {
+		float ratio1 = this.getX() / v.getX();
+		float ratio2 = this.getY() / v.getY();
+		float ratio3 = this.getZ() / v.getZ();
+		
+		return (ratio1 == ratio2) && (ratio1 == ratio3);
 	}
 	
 	
 }
+
