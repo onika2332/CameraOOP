4266d3f5831a9d09ae25d5cfabd675766bc22934
onika2332 <74920500+onika2332@users.noreply.github.com>
2021-05-31 19:27:47 +0700

    Update Point.java

--- model/Point.java
+++ model/Point.java
@@ -3,8 +3,14 @@ import georegression.struct.point.Point3D_F32;
 public class Point extends Point3D_F32 {
 	public State state;
 	
-	Point(State newState) {
+	public Point() {
 		super();
-		this.state = newState.Available;
+		this.state = State.Available;
+	}
+	
+	public Point(float x, float y, float z) {
+		super(x,y,z);
+		this.state = State.Available;
 	}
 }
+
